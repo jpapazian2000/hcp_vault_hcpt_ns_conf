ccd2f59f13921040af014d93c761bb8525a48ef6
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-24 18:54:14 +0200

    modify ns path

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "vault_admin_user" {
-  description = "user who will manage vault"
+  description = "user who will manage vault do not share outputs"
 }
 variable "vault_admin_password" {
   description = "password for the admin user of vault"
