ceaea0b60d9134806f983c537bfc16be92867635
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-24 18:06:02 +0200

    ns issue corrected

--- auth.tf
+++ auth.tf
@@ -6,7 +6,7 @@ data "tfe_outputs" "vault_infra" {
 #enable an auth method (userpass for the demo), and create a 'security admin' user
 #this user will perform tasks such as ns creation,
 resource "vault_auth_backend" "userpass" {
-  namespace = "dw"
+  namespace = "admin/dw"
   type = "userpass"
   #path = "chc_cybersecu"
 }
