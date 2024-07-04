ecaf67004fc67019e9cef9fb9183da1d5ac13c38
jerome papazian <j_papazian2000@yahoo.fr>
2024-07-04 16:29:19 +0200

    change ns names

--- ns.tf
+++ ns.tf
@@ -1,29 +1,29 @@
 resource "vault_namespace" "allergy_ns" {
-  path = "a"
+  path = "allergy"
   custom_metadata = {
     type = "allergy BU"
   }
 }
 resource "vault_namespace" "ccf_ns" {
-  path = "ccf"
+  path = "cough_cold_flu"
   custom_metadata = {
     type = "cough, clod and flu BU"
   }
 }
 resource "vault_namespace" "dw_ns" {
-  path = "dw"
+  path = "digestive_wellness"
   custom_metadata = {
     type = "digital wellness BU"
   }
 }
 resource "vault_namespace" "pc_ns" {
-  path = "pc"
+  path = "pain_care"
   custom_metadata = {
     type = "pain care BU"
   }
 }
 resource "vault_namespace" "pmw_ns" {
-  path = "pmw"
+  path = "physical_mental_wellness"
   custom_metadata = {
     type = "physical and mental wellness BU"
   }
