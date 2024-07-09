f8b22bd450671de5ea09de1cdfb183b2be8aa64d
jerome papazian <j_papazian2000@yahoo.fr>
2024-07-09 19:36:20 +0200

    connect to existing vault server

--- providers.tf
+++ providers.tf
@@ -6,9 +6,14 @@ terraform {
     }
   }
 }
+#data "tfe_outputs" "vault_infra" {
+   #organization = "jpapazian-org"
+   #workspace = "hcp_vault_hcpt_infra"
+#} 
 
 provider "vault" {
-  address = data.tfe_outputs.vault_infra.values.vault_public_url
+  #address = data.tfe_outputs.vault_infra.values.vault_public_url
+  address = var.vault_public_url
   auth_login_userpass {
     namespace = "admin"
     username = var.vault_admin_user
--- variables.tf
+++ variables.tf
@@ -4,3 +4,7 @@ variable "vault_admin_user" {
 variable "vault_admin_password" {
   description = "password for the admin user of vault"
 } 
+
+variable "vault_public_url" {
+  description = "url of the vault server"
+}
